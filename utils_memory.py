4e81bd2bfcbb65312574b0d87db0d039e8f588d1
Lanly109 <1094916227@qq.com>
2021-11-08 14:58:32 +0800

    use priority memory replay

--- utils_memory.py
+++ utils_memory.py
@@ -3,6 +3,7 @@ from typing import (
 )
 
 import torch
+import numpy as np
 
 from utils_types import (
     BatchAction,
@@ -13,6 +14,8 @@ from utils_types import (
     TensorStack5,
     TorchDevice,
 )
+
+from utils_sumtree import SumTree
 
 
 class ReplayMemory(object):
@@ -25,29 +28,34 @@ class ReplayMemory(object):
     ) -> None:
         self.__device = device
         self.__capacity = capacity
-        self.__size = 0
-        self.__pos = 0
-
-        self.__m_states = torch.zeros(
-            (capacity, channels, 84, 84), dtype=torch.uint8)
-        self.__m_actions = torch.zeros((capacity, 1), dtype=torch.long)
-        self.__m_rewards = torch.zeros((capacity, 1), dtype=torch.int8)
-        self.__m_dones = torch.zeros((capacity, 1), dtype=torch.bool)
+        self.tree = SumTree(capacity)
+        self.belta = 0.4
+        self.belta_increment_per_sampling = 0.001
+        # self.__size = 0
+        # self.__pos = 0
+        #
+        # self.__m_states = torch.zeros(
+        #     (capacity, channels, 84, 84), dtype=torch.uint8)
+        # self.__m_actions = torch.zeros((capacity, 1), dtype=torch.long)
+        # self.__m_rewards = torch.zeros((capacity, 1), dtype=torch.int8)
+        # self.__m_dones = torch.zeros((capacity, 1), dtype=torch.bool)
 
     def push(
             self,
+            p: float,
             folded_state: TensorStack5,
             action: int,
             reward: int,
             done: bool,
     ) -> None:
-        self.__m_states[self.__pos] = folded_state
-        self.__m_actions[self.__pos, 0] = action
-        self.__m_rewards[self.__pos, 0] = reward
-        self.__m_dones[self.__pos, 0] = done
-
-        self.__pos = (self.__pos + 1) % self.__capacity
-        self.__size = max(self.__size, self.__pos)
+        self.tree.add(p, (folded_state, action, reward, done))
+        # self.__m_states[self.__pos] = folded_state
+        # self.__m_actions[self.__pos, 0] = action
+        # self.__m_rewards[self.__pos, 0] = reward
+        # self.__m_dones[self.__pos, 0] = done
+        #
+        # self.__pos = (self.__pos + 1) % self.__capacity
+        # self.__size = max(self.__size, self.__pos)
 
     def sample(self, batch_size: int) -> Tuple[
             BatchState,
@@ -56,12 +64,27 @@ class ReplayMemory(object):
             BatchNext,
             BatchDone,
     ]:
-        indices = torch.randint(0, high=self.__size, size=(batch_size,))
-        b_state = self.__m_states[indices, :4].to(self.__device).float()
-        b_next = self.__m_states[indices, 1:].to(self.__device).float()
-        b_action = self.__m_actions[indices].to(self.__device)
-        b_reward = self.__m_rewards[indices].to(self.__device).float()
-        b_done = self.__m_dones[indices].to(self.__device).float()
+        pri_step = self.tree.total_p / batch_size
+        self.belta = np.min(1, self.belta + self.belta_increment_per_sampling)
+        min_p = self.tree.min_p / self.tree.total_p
+        min_p = 0.0001 if min_p == 0 else min_p
+        b_state = []
+        b_next = []
+        b_action = []
+        b_reward = []
+        b_done = []
+        ISweight = []
+        for i in range(batch_size):
+            st, ed = pri_step * i, pri_step * (i + 1)
+            v = torch.rand(st, ed, size=(1,))
+            indec, p, m_states, m_actions, m_rewards, m_dones = self.tree.get_leaf(v)
+
+            b_state.append(m_states[:4].to(self.__device).float())
+            b_next.append(m_states[1:].to(self.__device).float())
+            b_action.append(m_actions.to(self.__device))
+            b_reward.append(m_rewards.to(self.__device).float())
+            b_done.append(m_dones.to(self.__device).float())
+            ISweight.append(np.power(p / min_p, self.belta))
         return b_state, b_action, b_reward, b_next, b_done
 
     def __len__(self) -> int:
